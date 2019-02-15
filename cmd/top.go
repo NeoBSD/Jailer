4db96669a8b028ef5c40865a7b31d7b0775cc7e1
User Tobante <Tobias Hienzsch>
2019-02-15 21:14:43 +0100

    Fix top command output

--- cmd/top.go
+++ cmd/top.go
@@ -1,10 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"os/exec"
-	"time"
 
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -14,30 +13,18 @@ var topCmd = &cobra.Command{
 	Short: "Top inside a container",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Start a process:
-		c := exec.Command("top", "-jb", "2")
-		logrus.Warn(c.Args)
-		if err := c.Start(); err != nil {
-			logrus.Fatal(err)
+		// Setup external
+		externalCMD := "top"
+		c := exec.Command(externalCMD, "-jb", "2")
+
+		// Exec external
+		stdout, err := c.Output()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
 		}
 
-		// Wait for the process to finish or kill it after a timeout:
-		done := make(chan error, 1)
-		go func() {
-			done <- c.Wait()
-		}()
-		select {
-		case <-time.After(3 * time.Second):
-			if err := c.Process.Kill(); err != nil {
-				logrus.Fatal("failed to kill process: ", err)
-			}
-			logrus.Println("process killed as timeout reached")
-		case err := <-done:
-			if err != nil {
-				logrus.Fatalf("process finished with error = %v", err)
-			}
-			logrus.Print("process finished successfully")
-		}
+		fmt.Print(string(stdout))
 
 	},
 }
