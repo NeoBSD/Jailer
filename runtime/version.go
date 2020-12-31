826eedf3ac6cc547191cbf06fa86a9118af7a74f
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-01-01 00:56:22 +0100

    Cleanup

--- runtime/version.go
+++ runtime/version.go
@@ -1,13 +1,15 @@
 package runtime
 
-// Version is the sematic version of the binary
-var Version = "0.1.0"
+var (
+	// Version is the sematic version of the binary
+	Version = "0.1.0"
 
-// BuildCommit links the git commit at build
-var BuildCommit = "not set"
+	// BuildCommit links the git commit at build
+	BuildCommit = "not set"
 
-// BuildDate links current date at build
-var BuildDate = "not set"
+	// BuildDate links current date at build
+	BuildDate = "not set"
 
-// BuildOS links the host operating system name at build
-var BuildOS = "not set"
+	// BuildOS links the host operating system name at build
+	BuildOS = "not set"
+)
