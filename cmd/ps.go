1b26869b750aacff1fafe1c419ca64d15eb6d6cf
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-01-01 19:24:40 +0100

    Add fields to jail model

---
+++ cmd/ps.go
@@ -0,0 +1,148 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/NeoBSD/jailer/jail"
+	"github.com/spf13/cobra"
+)
+
+// psCmd represents the `ps` sub command
+var psCmd = &cobra.Command{
+	Use:   "ps",
+	Short: "List active jails",
+	Long:  ``,
+	RunE:  RunPsCommand,
+}
+
+// RunPsCommand executes the `ps` subcommand.
+func RunPsCommand(cmd *cobra.Command, args []string) error {
+	output, err := exec.Command("jls", "-n").Output()
+	if err != nil {
+		return err
+	}
+
+	jails, err := parseJLS(string(output))
+	if err != nil {
+		return err
+	}
+
+	js, err := json.Marshal(jails)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(js))
+
+	return nil
+}
+
+func parseJLS(str string) ([]jail.Jail, error) {
+	jails := []jail.Jail{}
+	lines := strings.Split(str, "\n")
+	for _, line := range lines {
+		// Split at spaces
+		items := strings.Split(line, " ")
+
+		// Ignore empty lines
+		if len(strings.Trim(line, " \n\r\t")) == 0 {
+			continue
+		}
+
+		j := jail.Jail{}
+
+		// Mapping
+		intValues := []struct {
+			ID    string
+			Value *int
+		}{
+			{ID: "jid", Value: &j.JID},
+			{ID: "parent", Value: &j.Parent},
+			{ID: "host.hostid", Value: &j.HostID},
+			{ID: "osreldate", Value: &j.OSReleaseDate},
+			{ID: "devfs_ruleset", Value: &j.DevFSRuleset},
+			{ID: "enforce_statfs", Value: &j.EnforceStatFS},
+			{ID: "securelevel", Value: &j.SecureLevel},
+			{ID: "children.cur", Value: &j.ChildrenCurrent},
+			{ID: "children.max", Value: &j.ChildrenMax},
+			{ID: "cpuset.id", Value: &j.CPUSetID},
+		}
+
+		stringValues := []struct {
+			ID    string
+			Value *string
+		}{
+			{ID: "name", Value: &j.Name},
+			{ID: "path", Value: &j.Path},
+			{ID: "host.hostname", Value: &j.Hostname},
+			{ID: "host.hostuuid", Value: &j.HostUUD},
+			{ID: "osrelease", Value: &j.OSRelease},
+		}
+
+		boolValues := []struct {
+			ID    string
+			Value *bool
+		}{
+			{ID: "allow.nochflags", Value: &j.AllowNochFlags},
+			{ID: "allow.nomlock", Value: &j.AllowNoMLock},
+			{ID: "allow.nomount", Value: &j.AllowNoMount},
+			{ID: "allow.mount.nodevfs", Value: &j.AllowMountNoDevFS},
+			{ID: "allow.mount.noprocfs", Value: &j.AllowMountNoProcFS},
+			{ID: "allow.mount.notmpfs", Value: &j.AllowMountNoTmpFS},
+			{ID: "allow.mount.nozfs", Value: &j.AllowMountNoZFS},
+			{ID: "allow.noquotas", Value: &j.AllowNoQuotas},
+			{ID: "allow.noraw_sockets", Value: &j.AllowNoRawSockets},
+			{ID: "allow.noread_msgbuf", Value: &j.AllowNoReadMsgBuf},
+			{ID: "allow.reserved_ports", Value: &j.AllowReservedPorts},
+			{ID: "allow.set_hostname", Value: &j.AllowSetHostname},
+			{ID: "allow.nosocket_af", Value: &j.AllowNoSocketAF},
+			{ID: "allow.nosysvipc", Value: &j.AllowNoSysVIPC},
+		}
+
+		// For each config item
+		for _, item := range items {
+			// Parse int values
+			for _, v := range intValues {
+				if strings.HasPrefix(item, v.ID) {
+					item = strings.TrimPrefix(item, v.ID)
+					item = strings.TrimPrefix(item, "=")
+					num, err := strconv.Atoi(item)
+					if err != nil {
+						return nil, err
+					}
+
+					*v.Value = num
+				}
+			}
+
+			// Parse string values
+			for _, v := range stringValues {
+				if strings.HasPrefix(item, v.ID) {
+					item = strings.TrimPrefix(item, v.ID)
+					item = strings.TrimPrefix(item, "=")
+					*v.Value = strings.Trim(item, " \n\t\r")
+				}
+			}
+
+			// Parse boolean values
+			for _, v := range boolValues {
+				if strings.HasPrefix(item, v.ID) {
+					*v.Value = true
+				}
+			}
+
+		}
+
+		jails = append(jails, j)
+	}
+
+	return jails, nil
+}
+
+func init() {
+	rootCmd.AddCommand(psCmd)
+}
--- jail/model.go
+++ jail/model.go
@@ -2,11 +2,62 @@ package jail
 
 // Jail represents a single jail config
 type Jail struct {
-	Name       string `json:"name"`
-	Hostname   string `json:"hostname"`
-	IP         string `json:"ip"`
-	Path       string `json:"path"`
-	MountDevFS bool   `json:"mount_devfs"`
-	ExecStart  string `json:"exec_start"`
-	ExecStop   string `json:"exec_stop"`
+	JID                int    `json:"jid" jail:"jid"`
+	Parent             int    `json:"parent" jail:"parent"`
+	Name               string `json:"name,omitempty" jail:"name"`
+	Hostname           string `json:"hostname,omitempty" jail:"host.hostname"`
+	HostID             int    `json:"hostid" jail:"host.hostid"`
+	HostUUD            string `json:"hostuuid,omitempty" jail:"host.hostuuid"`
+	OSRelease          string `json:"osrelease,omitempty" jail:"osrelease"`
+	OSReleaseDate      int    `json:"osreldate" jail:"osreldate"`
+	DevFSRuleset       int    `json:"devfs_ruleset" jail:"devfs_ruleset"`
+	EnforceStatFS      int    `json:"enforce_statfs" jail:"enforce_statfs"`
+	SecureLevel        int    `json:"securelevel" jail:"securelevel"`
+	ChildrenCurrent    int    `json:"children_current" jail:"children.cur"`
+	ChildrenMax        int    `json:"children_max" jail:"children.max"`
+	CPUSetID           int    `json:"cpuset_id" jail:"cpuset.id"`
+	IP                 string `json:"ip,omitempty" jail:"ip4.addr"`
+	Path               string `json:"path,omitempty" jail:"path"`
+	MountDevFS         bool   `json:"mount_devfs,omitempty" jail:"mount_devfs"`
+	ExecStart          string `json:"exec_start,omitempty" jail:"exec_start"`
+	ExecStop           string `json:"exec_stop,omitempty" jail:"exec_stop"`
+	AllowNochFlags     bool   `json:"allow_nochflags,omitempty" jail:"allow.nochflags"`
+	AllowNoMLock       bool   `json:"allow_nomlock,omitempty" jail:"allow.nomlock"`
+	AllowNoMount       bool   `json:"allow_nomount,omitempty" jail:"allow.nomount"`
+	AllowMountNoDevFS  bool   `json:"allow_mount_nodevfs,omitempty" jail:"allow.mount.nodevfs"`
+	AllowMountNoProcFS bool   `json:"allow_mount_noprocfs,omitempty" jail:"allow.mount.noprocfs"`
+	AllowMountNoTmpFS  bool   `json:"allow_mount_notmpfs,omitempty" jail:"allow.mount.notmpfs"`
+	AllowMountNoZFS    bool   `json:"allow_mount_nozfs,omitempty" jail:"allow.mount.nozfs"`
+	AllowNoQuotas      bool   `json:"allow_noquotas,omitempty" jail:"allow.noquotas"`
+	AllowNoRawSockets  bool   `json:"allow_noraw_sockets,omitempty" jail:"allow.noraw_sockets"`
+	AllowNoReadMsgBuf  bool   `json:"allow_noread_msgbuf,omitempty" jail:"allow.noread_msgbuf"`
+	AllowReservedPorts bool   `json:"allow_reserved_ports,omitempty" jail:"allow.reserved_ports"`
+	AllowSetHostname   bool   `json:"allow_set_hostname,omitempty" jail:"allow.set_hostname"`
+	AllowNoSocketAF    bool   `json:"allow_nosocket_af,omitempty" jail:"allow.nosocket_af"`
+	AllowNoSysVIPC     bool   `json:"allow_nosysvipc,omitempty" jail:"allow.nosysvipc"`
 }
+
+// =1202000
+// =0
+// =2
+// =-1
+// =0
+// children.max=0
+// =2
+// =0
+
+// nodying
+// host=new
+// ip4=inherit
+// ip6=inherit
+//  nopersist
+// sysvmsg=disable
+// sysvsem=disable
+// sysvshm=disable
+// vnet=inherit
+// host.domainname=/""}""
+// =test_01.tobante.local
+// =
+// ip4.saddrsel
+// ip6.addr=
+// ip6.saddrsel
