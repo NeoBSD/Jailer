a479a6d9cf1599f79960c8a2a094c6f033b71ce9
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-01-02 05:31:37 +0100

    compose: Delete dead code

--- cmd/jailer-compose/main.go
+++ cmd/jailer-compose/main.go
@@ -1,38 +1,3 @@
-// package main
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"os"
-
-// 	"github.com/NeoBSD/jailer"
-// )
-
-// func main() {
-// 	if err := run(); err != nil {
-// 		fmt.Println(err)
-// 		os.Exit(1)
-// 	}
-// }
-
-// func run() error {
-// 	path := "testdata/jailer-compose/jailer-compose.yml"
-// 	compose, err := jailer.ReadComposeFile(path)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// fmt.Println(compose)
-
-// 	js, err := json.Marshal(compose)
-// 	fmt.Println(string(js))
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
 package main
 
 import (
